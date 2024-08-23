bf93a84c94e11899d6761238af25bda772d433a4
vladimirmarych <marychev21@gmail.com>
2024-08-23 22:58:59 +0300

    init commit

---
+++ src/main/java/ru/vladimir/testmicroservicesdemo/TestMicroservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.vladimir.testmicroservicesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMicroservicesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMicroservicesDemoApplication.class, args);
+    }
+
+}
